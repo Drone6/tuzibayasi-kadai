48bae51c201eb606613a4ef92b8320cd3f03b689
Drone6 <h1201719043@hiratagakuen.onmicrosoft.com>
2018-04-17 13:11:50 +0900

    aa

---
+++ 辻林　課題/辻林　課題/Source.cpp
@@ -0,0 +1,43 @@
+#include"stdio.h"
+#include"conio.h"
+#include<math.h>
+int main(void)
+{
+	//int A = 8;
+	//5
+	//printf("%d", A >> 1);
+	//4
+	//printf("%d",1 << 2);
+
+	//3
+		float x;
+	float y;
+	x = cos(3.14 / 180 * 30);
+	y = sin(3.14 / 180 * 30);
+
+	printf("%f", x / y*0.5);
+
+	/*int num1=56;
+	int num2=256;*/
+	/*2500100“
+	HP‚ª1200*/
+
+	//2   0.48 *15
+	/*for (int i = 0; i<0.48*15;i++)
+	{
+	printf("š");
+
+	}*/
+
+
+	//printf("");
+
+
+	/*	printf("’Ò—Ñ—Á\n");
+	1	printf("%d\n",num1*100/num2);*/
+
+
+
+	_getch();
+	return 0;
+}
