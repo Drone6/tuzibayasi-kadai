d7d55f5a6bacad9f78ccfe12a16777cb5bd950ab
Drone6 <h1201719043@hiratagakuen.onmicrosoft.com>
2018-05-08 12:18:27 +0900

    a

---
+++ 辻林　課題/辻林　課題/kansucpp.cpp
@@ -0,0 +1,56 @@
+/*関数
+例
+int sub(int x)
+{
+int y=0;
+y=x*x;         処理　　　　　　　関数
+y++;
+return y;返す
+}
+
+void main()
+{
+int x=2;　　　　　　　　　　　　値が違うだけのアルゴリズムであれば同じ関数で処理させる
+int a=0;
+printf("%d,a);
+
+a=sub(3);
+printf("%d,a);
+}*/
+
+//a=sub(x);
+
+/*float Sub(int x,int y)
+{
+int z=o;
+for(int i=0;i<y;i++)
+z+=x;
+return z;
+}
+
+void main()
+{
+int a=0;
+a=sub(2,3)+sub(4,3);
+}
+
+
+問4入力した値が図の中であれば
+"OK"そうでなければ"NO"と出力しなさい
+*/
+#include<stdio.h>
+#include<conio.h>
+
+
+
+
+int sub(int x)
+{
+
+}
+int main(void)
+{
+
+}
+
+
